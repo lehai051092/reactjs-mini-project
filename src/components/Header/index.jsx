c05c6a50f644a340c71735d7b75580311dec7543
Hai Le <bibo051092@gmail.com>
2022-12-13 21:17:50 +0700

    Authentication Module - Header And Setup Package

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,36 +1,42 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import {AppBar} from "@mui/material";
-import CoronavirusIcon from '@mui/icons-material/Coronavirus';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import AcUnitIcon from '@material-ui/icons/AcUnit';
+import {Link} from "react-router-dom";
 
-Header.propTypes = {};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  link: {
+    color: '#fff',
+    textDecoration: "none"
+  }
+}));
 
-function Header(props) {
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
   return (
-    <Box sx={{flexGrow: 1}}>
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{mr: 2}}
-          >
-            <CoronavirusIcon/>
-            Mini Project
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{flexGrow: 1}}/>
+          <AcUnitIcon className={classes.menuButton}/>
+          <Typography variant="h6" className={classes.title}>
+            <Link className={classes.link} to="/">Mini Project</Link>
+          </Typography>
           <Button color="inherit">Register</Button>
         </Toolbar>
       </AppBar>
-    </Box>
+    </div>
   );
 }
-
-export default Header;
