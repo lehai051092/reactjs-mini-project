3c90ac4399df200e875fb430cc6a789cba5c2276
Hai Le <bibo051092@gmail.com>
2022-12-13 22:25:05 +0700

    Authentication Module - Style Form Register

--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -7,6 +7,25 @@ import {yupResolver} from "@hookform/resolvers/yup";
 import {Avatar} from "@material-ui/core";
 import {LockOutlined} from "@material-ui/icons";
 import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(4),
+  },
+  avatar: {
+    margin: "0 auto",
+    backgroundColor: theme.palette.secondary.main,
+  },
+  title: {
+    textAlign: "center",
+    margin: theme.spacing(2, 0, 3, 0),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2, 0),
+  }
+}));
 
 RegisterForm.propTypes = {
   onFormSubmit: PropTypes.func,
@@ -17,6 +36,7 @@ RegisterForm.defaultProps = {
 };
 
 function RegisterForm({onFormSubmit}) {
+  const classes = useStyles();
   const schema = yup.object({
     title: yup.string()
       .required('Please enter title')
@@ -42,11 +62,11 @@ function RegisterForm({onFormSubmit}) {
   }
 
   return (
-    <>
-      <Avatar>
+    <div className={classes.root}>
+      <Avatar className={classes.avatar}>
         <LockOutlined/>
       </Avatar>
-      <Typography component="h2" variant="h5">
+      <Typography className={classes.title} component="h2" variant="h5">
         Create An Account
       </Typography>
       <form onSubmit={form.handleSubmit(handleFormSubmit)}>
@@ -54,8 +74,11 @@ function RegisterForm({onFormSubmit}) {
         <InputField name="email" label="Email" form={form}/>
         <InputField name="password" label="Password" form={form}/>
         <InputField name="retypePassword" label="Retype Password" form={form}/>
+        <Button className={classes.submit} variant="contained" color="primary" fullWidth>
+          Sign Up
+        </Button>
       </form>
-    </>
+    </div>
   );
 }
 
