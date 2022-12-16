df9797f76dc16922da9bd0f8df2f002f799cc77c
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2022-12-16 10:12:57 +0700

    Product Feature - Style menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,41 +2,57 @@ import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import AcUnitIcon from '@material-ui/icons/AcUnit';
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import DialogUser from "./components/DialogUser";
+import IconButton from "@material-ui/core/IconButton";
+import {Box} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: "flex",
+    justifyContent: "space-between"
   },
   menuButton: {
     marginRight: theme.spacing(2),
-  },
-  title: {
     flexGrow: 1,
+    justifyContent: "flex-start",
+  },
+  navLink: {
+    display: "flex",
+    flexGrow: 10,
   },
   link: {
     color: '#fff',
-    textDecoration: "none"
-  }
+    textDecoration: "none",
+    padding: theme.spacing(1),
+    fontWeight: "500",
+  },
+  iconLink: {
+    display: "flex",
+    flexGrow: 1,
+    justifyContent: "flex-end",
+  },
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <AcUnitIcon className={classes.menuButton}/>
-          <Typography variant="h6" className={classes.title}>
-            <Link className={classes.link} to="/">Mini Project</Link>
-          </Typography>
+    <AppBar position="static">
+      <Toolbar className={classes.root}>
+        <IconButton className={classes.menuButton}>
+          <Link to="/" className={classes.link}>
+            <AcUnitIcon/>
+          </Link>
+        </IconButton>
+        <Box className={classes.navLink}>
+          <NavLink className={classes.link} to="/products">Products</NavLink>
+        </Box>
+        <Box className={classes.iconLink}>
           <DialogUser/>
-        </Toolbar>
-      </AppBar>
-    </div>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
