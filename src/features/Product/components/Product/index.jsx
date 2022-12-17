c351c0311ca701d3ee66954bcd5309eef26424a9
Hai Le <bibo051092@gmail.com>
2022-12-17 18:33:55 +0700

    Product Feature - Handle thumbnail product and format price VND

--- src/features/Product/components/Product/index.jsx
+++ src/features/Product/components/Product/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Box} from "@material-ui/core";
-import {Skeleton} from "@mui/material";
 import Typography from "@material-ui/core/Typography";
+import productImageDefault444 from "assets/images/product/444.png";
+import api from "../../../../constants/api";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(1),
+  },
+  imageBox: {
+    padding: theme.spacing(1),
+  },
+  price: {
+    fontWeight: "bold !important",
+    paddingRight: theme.spacing(1),
+  }
+}));
 
 Product.propTypes = {
   product: PropTypes.object,
@@ -13,14 +28,22 @@ Product.defaultProps = {
 }
 
 function Product({product}) {
+  const classes = useStyles();
+  const thumbnailUrl = product.thumbnail ? `${api.BASE_URL}${product.thumbnail?.url}` : productImageDefault444;
+
   return (
-    <Box padding={1}>
-      <Skeleton variant="rect" width="100%" height={118}/>
+    <Box className={classes.root}>
+      <Box className={classes.imageBox}>
+        <img src={thumbnailUrl} alt={product.name} width="100%"/>
+      </Box>
       <Typography variant="body2">
         {product.name}
       </Typography>
       <Typography variant="body2">
-        {product.salePrice} -{product.promotionPercent}
+        <Typography className={classes.price} component="span">
+          {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(product.salePrice)}
+        </Typography>
+        {product.promotionPercent !== 0 ? ` -${product.promotionPercent}%` : ''}
       </Typography>
     </Box>
   )
